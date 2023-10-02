40bcbfe973c506875451750e6c4d86f8ffac9b95
Jaejune <quswowns345@naver.com>
2023-10-02 16:05:08 +0900

    [2023/10/02] Test: 깃허브엑션 최종확인

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>식구(가제) 입니다.</p>
+        <p>안녕하세요.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
